1bfb3a9b4851516b7a29ac64d40db5091b32d01c
leifeng <helf@moutaiclub.org>
2023-06-30 15:25:35 +0800

    更改Header

--- lib_base/src/main/java/com/moufans/lib_base/request/net/HttpAddHeadersInterceptor.kt
+++ lib_base/src/main/java/com/moufans/lib_base/request/net/HttpAddHeadersInterceptor.kt
@@ -11,21 +11,15 @@ import okhttp3.Response
  * Created by leifeng on 2020/9/14.
  * 功能描述：请求头拦截器
  */
-open class HttpAddHeadersInterceptor : Interceptor {
+class HttpAddHeadersInterceptor : Interceptor {
     
-    @RequiresApi(Build.VERSION_CODES.CUPCAKE)
     override fun intercept(chain: Interceptor.Chain): Response {
         var request = chain.request()
         request = request.newBuilder().apply {
             // 主板
             addHeader("Device-Board", Build.BOARD)
-            addHeader("tenant-id ", "1")
         }.build()
         return chain.proceed(request)
     }
-    
-    open fun setHeader(request: Request.Builder){
-    
-    }
-    
+
 }
